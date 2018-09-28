3bf6a1ab9d2063c471ac58457629df508d7beba8
Adam Farden <adam@farden.cz>
2018-09-28 02:04:50 +0200

    Use EnableDelayedExpansion only in the 'function'

--- docker-entrypoint.cmd
+++ docker-entrypoint.cmd
@@ -1,20 +1,14 @@
 @echo off
-SETLOCAL EnableDelayedExpansion
 
 :: Batch file has no concept of a function, only goto
 goto :start
 
 :: usage: CALL :file_env VARIABLE [DEFAULT]
 ::    ie: CALL :file_env 'XYZ_DB_PASSWORD' 'example'
-::       (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-::       "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+::       (will allow for "%XYZ_DB_PASSWORD_FILE%" to fill in the value of
+::       "%XYZ_DB_PASSWORD%" from a file, especially for Docker's secrets feature)
 :file_env
-:: First, reset all variables
-set "cmdVar="
-set "fileVar="
-set "default="
-set "value="
-:: Now we can start the 'function'
+SETLOCAL EnableDelayedExpansion
 set cmdVar=%~1
 set fileVar=%cmdVar%_FILE
 set default=%~2
@@ -23,8 +17,7 @@ set default=%~2
 if NOT [!%cmdVar%!] == [] (
     if NOT [!%fileVar%!] == [] (
         :: Instead of exiting, just use the environment value
-        echo Warning: both %cmdVar% and %fileVar% are set
-        echo Warning: %cmdVar% will be used, %fileVar% will be ignored
+        echo Warning: both %cmdVar% and %fileVar% are set, %fileVar% will be ignored
     )
 )
 :: set as the default value
@@ -41,39 +34,46 @@ if [!%cmdVar%!] == [] (
         set /p value=<!%fileVar%!
     )
 )
-set %cmdVar%=%value%
+ENDLOCAL & (
+    set %cmdVar%=%value%
+)
 EXIT /B 0
+
+:: ------------------------------------------------------------
+:: ------------------------------------------------------------
+:: ------------------------------------------------------------
 
 :start
 
-:: Ensure the directories exists
+:: Ensure the directories exist with correct permissions
 if NOT exist %PGDATA% (
     mkdir %PGDATA%
 )
-call icacls "%PGDATA%" /grant "%USERNAME%":(OI)(CI)F
+call icacls "%PGDATA%" /grant "%USERNAME%":(OI)(CI)F > NUL
 if NOT exist %PGLOGS% (
     mkdir %PGLOGS%
 )
-call icacls "%PGLOGS%" /grant "%USERNAME%":(OI)(CI)F
+call icacls "%PGLOGS%" /grant "%USERNAME%":(OI)(CI)F > NUL
 
 :: look specifically for PG_VERSION, as it is expected in the DB dir
 if NOT exist "%PGC_DATA%\PG_VERSION" (
 
-    REM CALL :file_env POSTGRES_USER, postgres
-    REM CALL :file_env POSTGRES_PASSWORD
+    CALL :file_env POSTGRES_USER, postgres
+    CALL :file_env POSTGRES_PASSWORD
 
     :: TODO: find out why setting POSTGRES_PASSWORD as env works on Linux
     echo %POSTGRES_PASSWORD%> "%PGHOME%\.pgpass"
 
-    REM CALL :file_env POSTGRES_INITDB_ARGS
+    CALL :file_env POSTGRES_INITDB_ARGS
     if NOT [%POSTGRES_INITDB_WALDIR%] == [] (
         set POSTGRES_INITDB_ARGS=%POSTGRES_INITDB_ARGS% --waldir %POSTGRES_INITDB_WALDIR%
     )
     call initdb -U "%POSTGRES_USER%" -A md5 -E UTF8 --no-locale -D "%PGDATA%" --pwfile="%PGHOME%\.pgpass" %$POSTGRES_INITDB_ARGS% > "%PGLOGS%\install.log" 2>&1
 
+    :: Set a valid password file and delete the temporary password file
     echo localhost:%PGPORT%:*:%POSTGRES_USER%:%POSTGRES_PASSWORD%> %APPDATA%\postgresql\pgpass.conf
     echo 127.0.0.1:%PGPORT%:*:%POSTGRES_USER%:%POSTGRES_PASSWORD%>> %APPDATA%\postgresql\pgpass.conf
-    copy %PGHOME%\init\pg_hba.conf %PGDATA%\pg_hba.conf
+    copy %PGHOME%\init\pg_hba.conf %PGDATA%\pg_hba.conf > NUL
     call del "%PGHOME%\.pgpass"
 
     :: Set the initial connection environment
@@ -85,13 +85,13 @@ if NOT exist "%PGC_DATA%\PG_VERSION" (
     :: does not listen on external TCP/IP and waits until start finishes
 	call pg_ctl -D "%PGDATA%" -w start
 
-    set psqlOpt=^-v ON_ERROR_STOP=1 --no-password
+    set psqlOpt=^-v ON_ERROR_STOP=1
 
     if NOT [%POSTGRES_USER%] == [postgres] (
         echo CREATE DATABASE :"db"; | call psql %psqlOpt% --dbname postgres --set db="%POSTGRES_USER%"
         set PGDATABASE=%POSTGRES_USER%
     )
-    set psqlOpt=^-v ON_ERROR_STOP=1 --no-password --dbname "%PGDATABASE%"
+    set psqlOpt=^-v ON_ERROR_STOP=1 --dbname "%PGDATABASE%"
 
     :: Execute any SQL scripts for this new DB
     for %%f in (C:\docker-entrypoint-initdb.d\*.sql) do (
@@ -110,4 +110,4 @@ set PGDATABASE=%POSTGRES_USER%
 set PGPASSFILE=%APPDATA%\postgresql\pgpass.conf
 
 :: start the database
-REM call %*
+call %*
