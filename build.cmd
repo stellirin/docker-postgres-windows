50ea02b6c7b73efcbd8db62e57f7f10ebefc3122
Adam Farden <adam@farden.cz>
2018-09-29 14:28:33 +0200

    Reorder builds to push newest last

--- build.cmd
+++ build.cmd
@@ -5,36 +5,32 @@ SETLOCAL EnableDelayedExpansion
 
 set value=%~1
 if [%value%] == [] (
-    set pg11=true
-    set pg10=true
-    set pg96=true
-    set pg95=true
     set pg94=true
+    set pg95=true
+    set pg96=true
+    set pg10=true
+    set pg11=true
 )
 if NOT [%value%] == [] (
     set %value%=true
 )
 
-:: PostgeSQL 11
-if [%pg11%] == [true] (
+:: PostgeSQL 9.4
+if [%pg94%] == [true] (
     docker build ^
-        --build-arg PGC_DB=pg11 ^
-        --tag stellirin/postgres-windows:11 ^
+        --build-arg PGC_DB=pg94 ^
+        --tag stellirin/postgres-windows:9.4 ^
         .
-    docker push stellirin/postgres-windows:11
+    docker push stellirin/postgres-windows:9.4
 )
-    
-:: PostgeSQL 10
-if [%pg10%] == [true] (
+
+:: PostgeSQL 9.5
+if [%pg95%] == [true] (
     docker build ^
-        --build-arg PGC_DB=pg10 ^
-        --tag stellirin/postgres-windows:10.5 ^
-        --tag stellirin/postgres-windows:10 ^
-        --tag stellirin/postgres-windows:latest ^
+        --build-arg PGC_DB=pg95 ^
+        --tag stellirin/postgres-windows:9.5 ^
         .
-    docker push stellirin/postgres-windows:10.5
-    docker push stellirin/postgres-windows:10
-    docker push stellirin/postgres-windows:latest
+    docker push stellirin/postgres-windows:9.5
 )
 
 :: PostgeSQL 9.6
@@ -48,20 +44,24 @@ if [%pg96%] == [true] (
     docker push stellirin/postgres-windows:9
 )
 
-:: PostgeSQL 9.5
-if [%pg95%] == [true] (
+:: PostgeSQL 10
+if [%pg10%] == [true] (
     docker build ^
-        --build-arg PGC_DB=pg95 ^
-        --tag stellirin/postgres-windows:9.5 ^
+        --build-arg PGC_DB=pg10 ^
+        --tag stellirin/postgres-windows:10.5 ^
+        --tag stellirin/postgres-windows:10 ^
+        --tag stellirin/postgres-windows:latest ^
         .
-    docker push stellirin/postgres-windows:9.5
+    docker push stellirin/postgres-windows:10.5
+    docker push stellirin/postgres-windows:10
+    docker push stellirin/postgres-windows:latest
 )
 
-:: PostgeSQL 9.4
-if [%pg94%] == [true] (
+:: PostgeSQL 11
+if [%pg11%] == [true] (
     docker build ^
-        --build-arg PGC_DB=pg94 ^
-        --tag stellirin/postgres-windows:9.4 ^
+        --build-arg PGC_DB=pg11 ^
+        --tag stellirin/postgres-windows:11 ^
         .
-    docker push stellirin/postgres-windows:9.4
+    docker push stellirin/postgres-windows:11
 )
